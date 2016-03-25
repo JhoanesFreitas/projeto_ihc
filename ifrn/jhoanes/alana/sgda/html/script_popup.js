5e1540e881af18af279baffa0785bb2a8c21fa7a
JhoanesFreitas <jhoanesbarbosa@gmail.com>
2016-03-25 00:03:25 -0300

    adicionando evento do botao esc nos popup

---
+++ ifrn/jhoanes/alana/sgda/html/script_popup.js
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+
+var EXIT = 27;
+
+var ENTER = 13;
+
+function getKeyPress() {
+
+	try{
+		switch(event.keyCode){
+			case EXIT:
+			systemExit();
+			break;
+			default:
+			break;
+		}
+	} catch(e){
+		window.close();
+	}
+
+}
+
+function systemExit(){
+
+	try{
+		window.close();
+	}catch(e){
+		window.close();
+	}
+}
+
+</script>
